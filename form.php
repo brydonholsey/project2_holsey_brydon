b627c5e4d18fd4bc1d50639b1e003863c7648e3a
Brydon Holsey <brydon.holsey@ufl.edu>
2019-10-22 14:55:40 -0400

    update button styles and add form files

---
+++ form.php
@@ -0,0 +1,23 @@
+<!-- HTML top code goes here -->
+
+<?
+
+// From processing takes place here. message displayed here too
+require('form/form.class.php');
+$form = new Form();
+$form->processForm(
+
+	'brydonsholsey.com' // Put you mail domain here
+	,
+	'Brydon Holsey | Contact' // Put your site name / form name here
+	,
+	'brydon.holsey@gmail.com' // Where will the form notification be sent?
+	,
+	'noreply@brydonsholsey.com' // This what the form FROM: address wil be, if the form submissions doesn't contain an email field
+
+);
+
+
+?>
+
+<!-- HTML bottom code goes here -->
